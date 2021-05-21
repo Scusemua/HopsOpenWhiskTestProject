9b07df87ed1c16153c74e15f9fdf0bd0dbd345c6
Scusemua <benrcarver@gmail.com>
2021-05-21 15:10:25 -0400

    Created basic test.

---
+++ src/main/java/HttpPostInvokeTest.java
@@ -0,0 +1,49 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+import sun.net.www.http.HttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpPostInvokeTest {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        System.out.println("URL = " + args[0]);
+        HttpPost request = new HttpPost(args[0]);
+
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Authorization", "Basic Basic 789c46b1-71f6-4ed5-8c54-816aa4f8c502:abczO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP");
+
+        // Execute and get the response.
+        HttpResponse response = httpclient.execute(request);
+
+        String json = EntityUtils.toString(response.getEntity(), "UTF-8");
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+
+        System.out.println(jsonObject.toString());
+    }
+}
